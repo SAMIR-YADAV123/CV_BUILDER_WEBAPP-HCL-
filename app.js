767a9798a9799477045152446e7424b0ba90a626
SAMIR-YADAV123 <81609395+SAMIR-YADAV123@users.noreply.github.com>
2022-08-31 00:05:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,170 @@
+var skill_count=1;
+var hobby_count=1;
+var education_count=1;
+var experience_count=1;
+var project_count=1;
+function Download(){
+       var down=document.getElementById('pos');
+       down.style.visibility="hidden";
+       window.print();
+       down.style.visibility="visible";
+       window.alert("Download successfully");
+   }
+//    function Download(){
+//     var down=document.getElementById('pos1');
+//     down.style.visibility="hidden";
+//    var eml=document.getElementById('eml')
+//     function sendEmail() {
+//         Email.send({
+//         Host: "eml",
+//         Username : "<sender’s email address>",
+//         Password : "<email password>",
+//         To : '<recipient’s email address>',
+//         From : "<sender’s email address>",
+//         Subject : "<email subject>",
+//         Body : "<email body>",
+//         }).then(
+//             message => alert("mail sent successfully")
+//         );
+//     }
+
+
+//     down.style.visibility="visible";
+// }
+function addSkill(){
+    let addSkill = document.getElementById('addSkill');
+    let skillHide = document.getElementById('skill_hide');
+    if(skill_count<5)
+    {
+        ++skill_count;
+        var field = 
+        `<div class="mb-3">
+            <label class="form-label">Skillset Name</label>
+            <input type="text" class="form-control" name="skill${skill_count}">
+            <select class="form-select mt-2" name="skill_level${skill_count}">
+                <option value="">Select stars based upon your skill level</option>
+                <option value="1">1 - Novice</option>
+                <option value="2">2 - Advanced Beginner</option>
+                <option value="3">3 - Competent</option>
+                <option value="4">4 - Proficient</option>
+                <option value="5">5 - Expert</option>
+            </select>
+        </div>`;
+        console.log(field);
+        var htmlObject = document.createElement('div');
+        htmlObject.innerHTML=field;
+        addSkill.insertAdjacentElement("beforeend", htmlObject);
+    }
+    if(skill_count==5)
+    {
+        skillHide.style = "display:none";
+    }
+}
+
+function addHobby(){
+    let addHobby = document.getElementById('addHobby');
+    let hobbyHide = document.getElementById('hobby_hide');
+    if(hobby_count<4)
+    {
+        ++hobby_count;
+        var field = 
+        `<div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Hobby</label>
+            <input type="text" name="hobby${hobby_count}" class="form-control">
+        </div>`;
+        var htmlObject = document.createElement('div');
+        htmlObject.innerHTML=field;
+        addHobby.insertAdjacentElement("beforeend", htmlObject);
+    }
+    if(hobby_count==4)
+    {
+        hobbyHide.style = "display:none";
+    }
+}
+function addEducation(){
+    let addEducation = document.getElementById('addEducation');
+    let educationHide = document.getElementById('education_hide');
+    if(education_count<3)
+    {
+        ++education_count;
+        var field = 
+        `<div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">School/College/University</label>
+            <input type="text" name="institute${education_count}" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Degree Name</label>
+            <input type="text" name="degree${education_count}" class="form-control">
+        </div>
+        <div class="mb-3 d-flex justify-content-between">
+            <div>
+                <label for="exampleInputEmail1" class="form-label">From</label>
+                <input type="text" name="from${education_count}" class="form-control">
+            </div>
+            <div>
+                <label for="exampleInputEmail1" class="form-label">To</label>
+                <input type="text" name="to${education_count}" class="form-control">
+            </div>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Grade/Score/CGPA</label>
+            <input type="text" name="grade${education_count}" class="form-control">
+        </div>`;
+        var htmlObject = document.createElement('div');
+        htmlObject.innerHTML=field;
+        addEducation.insertAdjacentElement("beforeend", htmlObject);
+    }
+    if(education_count==3)
+    {
+        educationHide.style = "display:none";
+    }
+}
+function addProject(){
+    let addExperience = document.getElementById('addProject');
+    let experienceHide = document.getElementById('project_hide');
+    if(project_count<3)
+    {
+        ++project_count;
+        var field = 
+        `<div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Title</label>
+            <input type="text" name="ptitle${project_count}" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Description</label>
+            <input type="text" name="pdescription${project_count}" class="form-control">
+        </div>`;
+        var htmlObject = document.createElement('div');
+        htmlObject.innerHTML=field;
+        addExperience.insertAdjacentElement("beforeend", htmlObject);
+    }
+    if(project_count==3)
+    {
+        experienceHide.style = "display:none";
+    }
+}
+
+function addExperience(){
+    let addExperience = document.getElementById('addExperience');
+    let experienceHide = document.getElementById('experience_hide');
+    if(experience_count<3)
+    {
+        ++experience_count;
+        var field = 
+        `<div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Title</label>
+            <input type="text" name="title${experience_count}" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Description</label>
+            <input type="text" name="description${experience_count}" class="form-control">
+        </div>`;
+        var htmlObject = document.createElement('div');
+        htmlObject.innerHTML=field;
+        addExperience.insertAdjacentElement("beforeend", htmlObject);
+    }
+    if(experience_count==3)
+    {
+        experienceHide.style = "display:none";
+    }
+}
---
+++ submit.php
@@ -0,0 +1,331 @@
+<?php
+  define('Token','HGsZOXpfNC');
+  $skills = [];
+  $skill_levels = [];
+  $hobbies = [];
+  $institutes = [];
+  $degrees = [];
+  $froms = [];
+  $tos = [];
+  $grades = [];
+  $titles = [];
+  $descriptions = [];
+  $ptitles=[];
+  $pdescriptions=[];
+  if(Token == $_POST['token'])
+  {
+    $temp_profile = $_FILES['profile_image']['tmp_name'];
+    $profile = $_FILES['profile_image']['name'];
+    move_uploaded_file($temp_profile,'images/'.$profile);
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $linkdin =$_POST['linkdin'];
+    $profession = $_POST['profession'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $address=$_POST['address'];
+    $about_me = $_POST['about_me'];
+    array_push($skills,$_POST['skill1']);
+    array_push($skill_levels,intval($_POST['skill_level1']));
+    array_push($hobbies,$_POST['hobby1']);
+    array_push($institutes,$_POST['institute1']);
+    array_push($degrees,$_POST['degree1']);
+    array_push($froms,$_POST['from1']);
+    array_push($tos,$_POST['to1']);
+    array_push($grades,$_POST['grade1']);
+    array_push($titles,$_POST['title1']);
+    array_push($descriptions,$_POST['description1']);
+    array_push($ptitles,$_POST['ptitle1']);
+    array_push($pdescriptions,$_POST['pdescription1']);
+
+    if(isset($_POST['skill2']) && !empty($_POST['skill2']))
+    {
+      if(isset($_POST['skill_level2']) && !empty($_POST['skill_level2']))
+      {
+        array_push($skills,$_POST['skill2']);
+        array_push($skill_levels,intval($_POST['skill_level2']));
+      }
+    }
+    if(isset($_POST['skill3']) && !empty($_POST['skill3']))
+    {
+      if(isset($_POST['skill_level3']) && !empty($_POST['skill_level3']))
+      {
+        array_push($skills,$_POST['skill3']);
+        array_push($skill_levels,intval($_POST['skill_level3']));
+      }
+    }
+    if(isset($_POST['skill4']) && !empty($_POST['skill4']))
+    {
+      if(isset($_POST['skill_level4']) && !empty($_POST['skill_level4']))
+      {
+        array_push($skills,$_POST['skill4']);
+        array_push($skill_levels,intval($_POST['skill_level4']));
+      }
+    }
+    if(isset($_POST['skill5']) && !empty($_POST['skill5']))
+    {
+      if(isset($_POST['skill_level5']) && !empty($_POST['skill_level5']))
+      {
+        array_push($skills,$_POST['skill5']);
+        array_push($skill_levels,intval($_POST['skill_level5']));
+      }
+    }
+    if(isset($_POST['hobby2']) && !empty($_POST['hobby2']))
+    {
+      array_push($hobbies,$_POST['hobby2']);
+    }
+    if(isset($_POST['hobby3']) && !empty($_POST['hobby3']))
+    {
+      array_push($hobbies,$_POST['hobby3']);
+    }
+    if(isset($_POST['hobby4']) && !empty($_POST['hobby4']))
+    {
+      array_push($hobbies,$_POST['hobby4']);
+    }
+    if(isset($_POST['institute2']) && !empty($_POST['institute2']))
+    {
+      if(isset($_POST['degree2']) && !empty($_POST['degree2']))
+      {
+        if(isset($_POST['from2']) && !empty($_POST['from2']))
+        {
+          if(isset($_POST['to2']) && !empty($_POST['to2']))
+          {
+            if(isset($_POST['grade2']) && !empty($_POST['grade2']))
+            {
+              array_push($institutes,$_POST['institute2']);
+              array_push($degrees,$_POST['degree2']);
+              array_push($froms,$_POST['from2']);
+              array_push($tos,$_POST['to2']);
+              array_push($grades,$_POST['grade2']);
+            }
+          }
+        } 
+      }
+    }
+    if(isset($_POST['institute3']) && !empty($_POST['institute3']))
+    {
+      if(isset($_POST['degree3']) && !empty($_POST['degree3']))
+      {
+        if(isset($_POST['from3']) && !empty($_POST['from3']))
+        {
+          if(isset($_POST['to3']) && !empty($_POST['to3']))
+          {
+            if(isset($_POST['grade3']) && !empty($_POST['grade3']))
+            {
+              array_push($institutes,$_POST['institute3']);
+              array_push($degrees,$_POST['degree3']);
+              array_push($froms,$_POST['from3']);
+              array_push($tos,$_POST['to3']);
+              array_push($grades,$_POST['grade3']);
+            }
+          }
+        } 
+      }
+    }
+    if(isset($_POST['title2']) && !empty($_POST['title2']))
+    {
+      if(isset($_POST['description2']) && !empty($_POST['description2']))
+      {
+        array_push($titles,$_POST['title2']);
+        array_push($descriptions,$_POST['description2']);
+      }
+    }
+    if(isset($_POST['title3']) && !empty($_POST['title3']))
+    {
+      if(isset($_POST['description3']) && !empty($_POST['description3']))
+      {
+        array_push($titles,$_POST['title3']);
+        array_push($descriptions,$_POST['description3']);
+      }
+    }
+
+    if(isset($_POST['ptitle2']) && !empty($_POST['ptitle2']))
+    {
+      if(isset($_POST['pdescription2']) && !empty($_POST['pdescription2']))
+      {
+        array_push($ptitles,$_POST['ptitle2']);
+        array_push($pdescriptions,$_POST['pdescription2']);
+      }
+    }
+    if(isset($_POST['ptitle3']) && !empty($_POST['ptitle3']))
+    {
+      if(isset($_POST['pdescription3']) && !empty($_POST['pdescription3']))
+      {
+        array_push($ptitles,$_POST['ptitle3']);
+        array_push($pdescriptions,$_POST['pdescription3']);
+      }
+    }
+  }
+  else
+  {
+    header('location: /resumegenerator');
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+  <link rel="stylesheet" href="style.css">
+  <title><?php echo ucwords($first_name). ' Resume'; ?></title>
+</head>
+<body>
+  <button class="sbtbtn" id="pos" onclick="Download()">Download</button>
+  
+  
+  <!-- <a href="mailto:${$email}"
+   title="Share by Email">
+  <button class="btbtn" id="pos1" onclick="emailshare()">Share to Email</button>
+
+</a> -->
+
+
+  <div class="grid-container">
+  <div class="zone-1">
+    <div class="toCenter">
+      <img src="images/<?php echo $profile;?>" class="profile">
+    </div>
+    <div class="contact-box">
+      <div class="title">
+        <h2>Contact</h2>
+        <br>
+        <hr style="background-color:black;width:200px">
+      </div>
+      <div class="call"><i class="fas fa-home"></i>
+        <div class="text"><?php echo $address;?></div>
+      </div>
+      <div class="call"><i class="fas fa-phone-alt"></i>
+        <div class="text"><?php echo $phone;?></div>
+      </div>
+      <div class="email"><i class="fas fa-envelope"></i>
+        <div id="eml" class="text"><?php echo $email;?></div>
+      </div>
+      <div class="email"><i class="fab fa-linkedin"></i>
+        <div class="text"><a target="_blank" href="{$linkdin}"><?php echo $linkdin;?></a></div>
+      </div>
+    </div>
+    <div class="personal-box">
+      <div class="title">
+        <h2>Skills</h2>
+        <hr style="background-color:black">
+      </div>
+      <?php 
+      for($j=0; $j<count($skills); $j++){
+          echo "<div class='skill-1'>
+          
+                  <p ><strong>" . strtoupper($skills[$j]) . "</strong></p>
+                  <div class='progress'>";
+                  
+            for($i=0;$i<$skill_levels[$j];$i++){
+              echo '<div class="fas fa-star active"></div>';              
+            }
+            echo '</div></div>';
+
+          }
+      ?>
+    </div>
+    <div class="hobbies-box">
+      <div class="title">
+        <h2>Hobbies</h2>
+        <hr style="background-color:black">
+      </div>
+      <?php 
+        foreach($hobbies as $hobby)
+        {
+          echo "<div class='d-flex align-items-center'>
+          <div class='circle'></div>
+          <div><strong>" . ucwords($hobby) . "</strong></div>
+        </div>";
+        }
+      
+      
+      ?>
+    </div>
+  </div>
+  <div class="zone-2">
+    <div class="headTitle">
+      <h1><?php echo ucwords($first_name);?><br><b><?php echo ucwords($last_name);?></b></h1>
+    </div>
+    <div class="subTitle">
+      <h1><?php echo ucwords($profession);?><h1>
+    </div>
+    <div class="group-1">
+      <div class="title">
+        <div class="box">
+          <h2>Profile</h2>
+        </div>
+      </div>
+      <div class="desc"><?php echo $about_me;?></div>
+    </div>
+    <div class="group-2">
+      <div class="title">
+        <div class="box">
+          <h2>Education</h2>
+        </div>
+      </div>
+      <div class="desc">
+        <?php 
+          for($i=0; $i<count($institutes);$i++)
+          {
+            echo "<ul>
+            <li>
+            <div id='edu' class='msg-1 '>" . ucwords($institutes[$i]) ."   ->   ".$froms[$i] . "-" . $tos[$i]. "</div>
+              <div id='des' class='msg-2'>". ucwords($degrees[$i]) . "  |  ". $grades[$i]. "%" ."</div>
+             
+            </li>
+          </ul>";
+          }
+        ?>
+      </div>
+    </div>
+    <div class="group-3">
+      <div class="title">
+        <div class="box">
+          <h2>Experience</h2>
+        </div>
+      </div>
+      <div class="desc">
+      <?php 
+          for($i=0; $i<count($titles);$i++)
+          {
+            echo "<ul>
+            <li>
+              <div class='msg-1'><br></div>
+              <div class='msg-2'>" . ucwords($titles[$i]) ."</div>
+              <div class='msg-3'>" . ucfirst($descriptions[$i]) . "</div>
+            </li>
+          </ul>";
+          }
+        ?>
+      </div>
+    </div>
+  
+    <div class="group-4">
+      <div class="title">
+        <div class="box">
+          <h2>Projects</h2> 
+        </div>
+      </div>
+      <div class="desc">
+      <?php  
+         for($i=0; $i<count($ptitles);$i++)
+          {
+            echo "<ul>
+            <li>
+              <div class='msg-1'><br></div>
+              <div class='msg-2'>" . ucwords($ptitles[$i]) ."</div>
+              <div class='msg-3'>" . ucfirst($pdescriptions[$i]) . "</div>
+            </li>
+          </ul>";
+          }
+        ?>
+      </div>
+    </div>
+        
+  </div>
+</div>
+<script src="app.js"></script>
+</body>
+</html>

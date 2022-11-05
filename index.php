5332ce0b4483801c5cac5228bad4c397398d02dd
SAMIR-YADAV123 <81609395+SAMIR-YADAV123@users.noreply.github.com>
2022-11-05 20:34:25 +0530

    Initial commit

--- index.php
+++ index.php
@@ -1,184 +1,62 @@
+<?php
+@include 'config.php';
+// $conn = mysqli_connect('localhost','root','','userlogin');
+// mysqli_connect('localhost','root','','userlogin');
+
+if(isset($_POST['submit'])){
+
+   $email = $_POST['email'];
+   $pass = $_POST['password'];
+
+
+   $quer=$conn->prepare("select * from user_form where email = ?");
+   $quer->bind_param("s",$email);
+   $quer->execute(); 
+   $result=$quer->get_result();
+
+   if($result->num_rows > 0){
+      header('location:form.php');
+   }
+   else{
+      $error[] = 'incorrect email or password!';
+   }
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resume Form</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>login form</title>
 
-    <style>
-        body{
-            /* background: linear-gradient(to right,#228082,#f5cd88); */
-             background:#3f9ea5;
-        }
-    </style>
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
 </head>
 <body>
-    <div class="container text-light">
-    <h1 class="text-center my-5 fw-bold">Resume Building Form</h1>
-    <div class="form-container">
-        <form action="submit.php" method="POST" enctype="multipart/form-data">
-            <input type="hidden" name="token" value="HGsZOXpfNC">
-            <div class="border border-dark p-3 mb-3">    
-                <h2>Profile Image</h2>
-                <div class="mb-3">
-                    <label class="form-label">Select a square image</label>
-                    <input class="form-control" name="profile_image" type="file" required>
-                </div>
-            </div>
-            <div class="border border-dark p-3 mb-3">    
-                <h2>Contact</h2>
-                <div class="d-flex justify-content-between mb-3">
-                    <div>
-                        <label class="form-label">First Name</label>
-                        <input type="text" name="first_name" class="form-control" required>
-                    </div>
-                    <div>
-                        <label class="form-label">Last Name</label>
-                        <input type="text" name="last_name" class="form-control" required>
-                    </div>
-                   
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Profession</label>
-                    <input type="text" class="form-control" name="profession" required>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Address</label>
-                    <input type="text" class="form-control" name="address" required>
-                    <!-- <div class="form-text text-light">We'll never share your email with anyone else.</div> -->
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Email address</label>
-                    <input type="email" class="form-control" name="email" required>
-                    <!-- <div class="form-text text-light">We'll never share your email with anyone else.</div> -->
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Phone number</label>
-                    <input type="tel" class="form-control" id="phone" name="phone" placeholder="03001234567" required>
-                </div>
-                <div>
-                        <label class="form-label">LinkDin</label>
-                        <input type="text" name="linkdin" class="form-control" required>
-                    </div>
-            </div>
-            <div class="border border-dark p-3 mb-3">    
-                <h2>Skills </h2>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Skillset Name</label>
-                    <input type="text" class="form-control" name="skill1" required>
-                    <select class="form-select mt-2" name="skill_level1" required>
-                        <option value="">Select stars based upon your skill level</option>
-                        <option value="1">1 - Novice</option>
-                        <option value="2">2 - Advanced Beginner</option>
-                        <option value="3">3 - Competent</option>
-                        <option value="4">4 - Proficient</option>
-                        <option value="5">5 - Expert</option>
-                    </select>
-                </div>
-                <div id="addSkill"></div>
-                <div class="mb-3">
-                    <button type="button" id="skill_hide" class="btn btn-primary form-control" onclick="addSkill()">+</button>
-                </div>
-            </div>
-            
-            <div class="border border-dark p-3 mb-3">    
-                <h2>About Me</h2>
-                <div class="form-floating">
-                    <textarea class="form-control" name="about_me" style="height: 100px" required></textarea>
-                </div>
-            </div>
-            <div class="border border-dark p-3 mb-3">    
-                <h2>Education </h2>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">School/College/University</label>
-                    <input type="text" name="institute1" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Degree Name</label>
-                    <input type="text" name="degree1" class="form-control">
-                </div>
-                <div class="mb-3 d-flex justify-content-between">
-                    <div>
-                        <label for="exampleInputEmail1" class="form-label">From</label>
-                        <input type="text" name="from1" class="form-control">
-                    </div>
-                    <div>
-                        <label for="exampleInputEmail1" class="form-label">To</label>
-                        <input type="text" name="to1" class="form-control">
-                    </div>
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Grade/Score/CGPA</label>
-                    <input type="text" name="grade1" class="form-control">
-                </div>
-                <div id="addEducation"></div>
-                <div class="mb-3">
-                    <button type="button" id="education_hide" class="btn btn-primary form-control" onclick="addEducation()">+</button>
-                </div>
-            </div>
-            <div class="border border-dark p-3 mb-3">    
-                <h2>Experience</h2>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Title</label>
-                    <input type="text" name="title1" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Description</label>
-                    <input type="text" name="description1" class="form-control">
-                </div>
-                <div id="addExperience"></div>
-                <div class="mb-3">
-                    <button type="button" id="experience_hide" class="btn btn-primary form-control" onclick="addExperience()">+</button>
-                </div>
-            </div>
-            <div class="border border-dark p-3 mb-3">    
-                <h2>Projects</h2>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Title</label>
-                    <input type="text" name="ptitle1" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Description</label>
-                    <input type="text" name="pdescription1" class="form-control">
-                </div>
-                <div id="addProject"></div>
-                <div class="mb-3">
-                    <button type="button" id="project_hide" class="btn btn-primary form-control" onclick="addProject()">+</button>
-                </div>
-            </div>
-            <div class="border border-dark p-3 mb-3">    
-                <h2>Languages</h2>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">language</label>
-                    <input type="text" name="lang1" class="form-control" required>
-                </div>
-                <div id="addlang"></div>
-                <div class="mb-3">
-                    <button type="button" id="lang_hide" class="btn btn-primary form-control" onclick="addLange()">+</button>
-                </div>
-            </div>
-            <div class="border border-dark p-3 mb-3">    
-                <h2>Hobbies</h2>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Hobby</label>
-                    <input type="text" name="hobby1" class="form-control" required>
-                </div>
-                <div id="addHobby"></div>
-                <div class="mb-3">
-                    <button type="button" id="hobby_hide" class="btn btn-primary form-control" onclick="addHobby()">+</button>
-                </div>
-            </div>
+   
+<div class="form-container">
 
-           
+   <form action="" method="post">
+      <h3>login now</h3>
+      <?php
+      if(isset($error)){
+         foreach($error as $error){
+            echo '<span class="error-msg">'.$error.'</span>';
+         };
+      };
+      ?>
+      <input type="email" name="email" required placeholder="enter your email">
+      <input type="password" name="password" required placeholder="enter your password">
+      <input type="submit" name="submit" value="login now" class="form-btn">
+      <p>don't have an account? <a href="register.php">register now</a></p>
+      
+   </form>
 
+</div>
 
-            <input type="submit" class="form-control my-2">
-        </form>
-    </div>
-    </div>
-    <script src="app.js"></script>
 </body>
 </html>
